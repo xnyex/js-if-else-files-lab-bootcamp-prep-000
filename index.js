2432afb42f98ebc40043cd6674b3cc2830fce9e9
Nye Smith <nyedess@gmail.com>
2018-05-08 17:23:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-var name = "Susan"
-if (name === "Susan") {
-  console.log("The name is correct")
-}
-else {
-  console.log("Expected the name to be Susan")
-}
-var height = "74"
-
-
-// Don't worry about this
-module.exports = { name, height
-}
-
+829 W Carpenter 
